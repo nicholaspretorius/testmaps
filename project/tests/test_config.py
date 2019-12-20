80f1e1fbaecb364cb0162d74e8f8bbeef7946cc8
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-12-20 09:05:07 +0200

    tests: Add test setup and config/hello tests

---
+++ project/tests/test_config.py
@@ -0,0 +1,24 @@
+import os
+
+
+def test_development_config(test_app):
+    test_app.config.from_object("project.config.DevelopmentConfig")
+    assert test_app.config["SECRET_KEY"] == "test"
+    assert not test_app.config["TESTING"]
+    assert test_app.config["SQLALCHEMY_DATABASE_URI"] == os.environ.get("DATABASE_URL")
+
+
+def test_testing_config(test_app):
+    test_app.config.from_object("project.config.TestingConfig")
+    assert test_app.config["SECRET_KEY"] == "test"
+    assert test_app.config["TESTING"]
+    assert test_app.config["SQLALCHEMY_DATABASE_URI"] == os.environ.get(
+        "DATABASE_TEST_URL"
+    )
+
+
+def test_production_config(test_app):
+    test_app.config.from_object("project.config.ProductionConfig")
+    assert test_app.config["SECRET_KEY"] == "test"
+    assert not test_app.config["TESTING"]
+    assert test_app.config["SQLALCHEMY_DATABASE_URI"] == os.environ.get("DATABASE_URL")
---
+++ project/tests/test_hello.py
@@ -0,0 +1,10 @@
+import json
+
+
+def test_hello(test_app):
+    client = test_app.test_client()
+    res = client.get("/hello/")
+    data = json.loads(res.data.decode())
+    assert res.status_code == 200
+    assert "success" in data["status"]
+    assert "world!" in data["hello"]
