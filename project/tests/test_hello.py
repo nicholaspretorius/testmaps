c866dcbbc7c36f0b920f1ba2ab72ed978682cf66
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-12-20 10:19:54 +0200

    tests: Fix tests, add prefix for routes

--- project/tests/test_hello.py
+++ project/tests/test_hello.py
@@ -1,9 +1,11 @@
 import json
 
+prefix = "/api/1"
+
 
 def test_hello(test_app):
     client = test_app.test_client()
-    res = client.get("/hello/")
+    res = client.get(f"{prefix}/hello/")
     data = json.loads(res.data.decode())
     assert res.status_code == 200
     assert "success" in data["status"]
