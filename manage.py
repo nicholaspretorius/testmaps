f3e70f6e2fc4d153241eecf8390a68b47764750a
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-12-30 15:21:26 +0200

    feat: Combined db tasks

--- manage.py
+++ manage.py
@@ -10,6 +10,7 @@ cli = FlaskGroup(create_app=create_app)
 
 @cli.command("recreate_db")
 def recreate_db():
+    """Recreates the database."""
     db.drop_all()
     db.create_all()
     db.session.commit()
@@ -18,6 +19,16 @@ def recreate_db():
 @cli.command("seed_db")
 def seed_db():
     """Seeds the database."""
+    db.session.add(User(email="test@test.com"))
+    db.session.add(User(email="another@test.com"))
+    db.session.commit()
+
+
+@cli.command("reset_db")
+def reset_db():
+    """Recreates and seeds the database."""
+    db.drop_all()
+    db.create_all()
     db.session.add(User(email="test@test.com"))
     db.session.add(User(email="another@test.com"))
     db.session.commit()
