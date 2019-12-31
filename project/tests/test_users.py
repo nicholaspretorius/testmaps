0b970c88dc6e9018111ca6008ea58338b7141919
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-12-31 11:18:56 +0200

    tests: Updated tests

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -3,10 +3,10 @@ import json
 prefix = "/api/1"
 
 
-def test_add_user(test_app, test_database):
+def test_add_user(test_app, test_db):
     client = test_app.test_client()
     res = client.post(
-        f"{prefix}/users",
+        f"{prefix}/users/",
         data=json.dumps({"email": "test@test.com"}),
         content_type="application/json",
     )
@@ -15,3 +15,50 @@ def test_add_user(test_app, test_database):
     assert res.status_code == 201
     assert "test@test.com was added" in data["message"]
     assert "success" in data["status"]
+
+
+def test_add_user_no_post_data(test_app, test_db):
+    client = test_app.test_client()
+    res = client.post(
+        f"{prefix}/users/", data=json.dumps({}), content_type="application/json",
+    )
+
+    data = json.loads(res.data.decode())
+    assert res.status_code == 400
+    assert "fail" in data["status"]
+    assert "Invalid payload" in data["message"]
+
+
+def test_add_user_no_email(test_app, test_db):
+    client = test_app.test_client()
+    res = client.post(
+        f"{prefix}/users/",
+        data=json.dumps({"website": "test@test.com"}),
+        content_type="application/json",
+    )
+
+    data = json.loads(res.data.decode())
+    assert res.status_code == 400
+    assert "fail" in data["status"]
+    assert "Invalid payload" in data["message"]
+
+
+def test_add_user_duplicate_email(test_app, test_db):
+    client = test_app.test_client()
+
+    client.post(
+        f"{prefix}/users/",
+        data=json.dumps({"email": "test@test.com"}),
+        content_type="application/json",
+    )
+
+    res = client.post(
+        f"{prefix}/users/",
+        data=json.dumps({"email": "test@test.com"}),
+        content_type="application/json",
+    )
+
+    data = json.loads(res.data.decode())
+    assert res.status_code == 400
+    assert "fail" in data["status"]
+    assert "Sorry, that email already exists." in data["message"]
