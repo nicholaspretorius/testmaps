26717118c6c50aa87020a90e39fb70e4296b2f31
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-12-31 13:46:32 +0200

    tests: Added utils and refactored some test logic

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -1,7 +1,6 @@
 import json
 
-from project import db
-from project.apis.models import User
+from project.tests.utils import add_user, recreate_db
 
 prefix = "/api/1"
 
@@ -10,14 +9,14 @@ def test_add_user(test_app, test_db):
     client = test_app.test_client()
     res = client.post(
         f"{prefix}/users/",
-        data=json.dumps({"email": "test@test.com"}),
+        data=json.dumps({"email": "test1@test.com"}),
         content_type="application/json",
     )
 
     data = json.loads(res.data.decode())
     assert res.status_code == 201
-    assert "test@test.com was added" in data["message"]
-    assert "success" in data["status"]
+    assert "test1@test.com was added" in data["message"]
+    assert data["status"]
 
 
 def test_add_user_no_post_data(test_app, test_db):
@@ -28,7 +27,7 @@ def test_add_user_no_post_data(test_app, test_db):
 
     data = json.loads(res.data.decode())
     assert res.status_code == 400
-    assert "fail" in data["status"]
+    assert not data["status"]
     assert "Invalid payload" in data["message"]
 
 
@@ -36,34 +35,35 @@ def test_add_user_no_email(test_app, test_db):
     client = test_app.test_client()
     res = client.post(
         f"{prefix}/users/",
-        data=json.dumps({"website": "test@test.com"}),
+        data=json.dumps({"website": "website@test.com"}),
         content_type="application/json",
     )
 
     data = json.loads(res.data.decode())
     assert res.status_code == 400
-    assert "fail" in data["status"]
+    assert not data["status"]
     assert "Invalid payload" in data["message"]
 
 
 def test_add_user_duplicate_email(test_app, test_db):
+    recreate_db()
     client = test_app.test_client()
 
     client.post(
         f"{prefix}/users/",
-        data=json.dumps({"email": "test@test.com"}),
+        data=json.dumps({"email": "test2@test.com"}),
         content_type="application/json",
     )
 
     res = client.post(
         f"{prefix}/users/",
-        data=json.dumps({"email": "test@test.com"}),
+        data=json.dumps({"email": "test2@test.com"}),
         content_type="application/json",
     )
 
     data = json.loads(res.data.decode())
     assert res.status_code == 400
-    assert "fail" in data["status"]
+    assert not data["status"]
     assert "Sorry, that email already exists." in data["message"]
 
 
@@ -78,21 +78,20 @@ def test_add_user_invalid_email(test_app, test_db):
 
     data = json.loads(res.data.decode())
     assert res.status_code == 400
-    assert "fail" in data["status"]
+    assert not data["status"]
     assert "Please provide a valid email address" in data["message"]
 
 
 def test_single_user(test_app, test_db):
-    user = User(email="test@test.com")
-    db.session.add(user)
-    db.session.commit()
+    recreate_db()
+    user = add_user("test3@test.com")
 
     client = test_app.test_client()
     res = client.get(f"{prefix}/users/{user.id}")
     data = json.loads(res.data.decode())
     assert res.status_code == 200
     assert str(user.id) in data["id"]
-    assert "test@test.com" in data["email"]
+    assert "test3@test.com" in data["email"]
 
 
 def test_single_user_not_found(test_app, test_db):
@@ -100,8 +99,8 @@ def test_single_user_not_found(test_app, test_db):
     res = client.get(f"{prefix}/users/999")
     data = json.loads(res.data.decode())
     assert res.status_code == 404
-    assert "fail" in data["status"]
-    assert "User does not exist" in data["message"]
+    assert not data["status"]
+    assert "Resource not found" in data["message"]
 
 
 def test_single_user_no_id(test_app, test_db):
@@ -109,5 +108,19 @@ def test_single_user_no_id(test_app, test_db):
     res = client.get(f"{prefix}/users/test")
     data = json.loads(res.data.decode())
     assert res.status_code == 404
-    assert "fail" in data["status"]
-    assert "User does not exist" in data["message"]
+    assert not data["status"]
+    assert "Resource not found" in data["message"]
+
+
+def test_get_all_users(test_app, test_db):
+    recreate_db()
+    client = test_app.test_client()
+    add_user("test4@test.com")
+    add_user("test5@test.com")
+
+    res = client.get(f"{prefix}/users/")
+    data = json.loads(res.data.decode())
+    assert res.status_code == 200
+    assert len(data) == 2
+    assert "test4@test.com" in data[0]["email"]
+    assert "test5@test.com" in data[1]["email"]
---
+++ project/tests/utils.py
@@ -0,0 +1,15 @@
+from project import db
+from project.apis.models import User
+
+
+def add_user(email):
+    user = User(email=email)
+    db.session.add(user)
+    db.session.commit()
+    return user
+
+
+def recreate_db():
+    db.session.remove()
+    db.drop_all()
+    db.create_all()
