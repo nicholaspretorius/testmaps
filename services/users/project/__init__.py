b4289af9494f3031fb25bc1538a8264b30ac1f65
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-01-02 10:05:19 +0200

    feat: Setup flask-bcrypt for use in app

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, jsonify
 from flask_admin import Admin
+from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,6 +11,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 cors = CORS()
+bcrypt = Bcrypt()
 admin = Admin(template_mode="bootstrap3")
 
 
@@ -17,23 +19,29 @@ def create_app(script_info=None):
 
     app = Flask(__name__)
 
+    # set config
     app_settings = os.getenv("APP_SETTINGS")
     app.config.from_object(app_settings)
 
+    # setup extensions
     db.init_app(app)
     cors.init_app(app, resources={r"*": {"origins": "*"}})
+    bcrypt.init_app(app)
 
     if os.getenv("FLASK_ENV") == "development":
         admin.init_app(app)
 
+    # register api
     from project.apis import blueprint as api
 
     app.register_blueprint(api, url_prefix=app.config["API_PREFIX"])
 
+    # shell context for flask cli
     @app.shell_context_processor
     def ctx():
         return {"app": app, "db": db}
 
+    # error handlers
     @app.errorhandler(404)
     def not_found(error):
         return jsonify({"status": False, "message": "Resource not found"}), 404
