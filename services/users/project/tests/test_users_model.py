f8d7862c98b0fae569173466d9449ef4ab9c4419
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-01-03 18:14:00 +0200

    tests: Add test for repr

--- services/users/project/tests/test_users_model.py
+++ services/users/project/tests/test_users_model.py
@@ -3,6 +3,12 @@ from project.apis.users.models import User
 prefix = "/api/1"
 
 
+def test_repr(test_app, test_db, add_user):
+    user = add_user(email="test@test.com", password="password")
+    val = repr(user)
+    assert val == "<User id: 1, email: test@test.com>"
+
+
 def test_password_are_random(test_app, test_db, add_user):
     user_one = add_user("test@test.com", "password")
     user_two = add_user("test2@test.com", "password")
@@ -13,6 +19,9 @@ def test_encode_token(test_app, test_db, add_user):
     user = add_user("test@test.com", "password")
     token = user.encode_token(user.id, "access")
     assert isinstance(token, bytes)
+
+    token_two = user.encode_token(user.id, "refresh")
+    assert isinstance(token_two, bytes)
 
 
 def test_decode_token(test_app, test_db, add_user):
