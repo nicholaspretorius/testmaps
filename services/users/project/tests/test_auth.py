e85b5ae4ec24af880a88177fbb0fe63dc99d77d3
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-01-04 11:27:07 +0200

    tests: Fix use of parametrize, add login tests

--- services/users/project/tests/test_auth.py
+++ services/users/project/tests/test_auth.py
@@ -41,17 +41,20 @@ def test_user_registration_duplicate_email(test_app, test_db, add_user):
 
 
 @pytest.mark.parametrize(
-    "payload",
+    "payload, message",
     [
-        [{}],
-        [{"email": "test", "password": "password"}],
-        [{"email": "test@test.com"}],
-        [{"password": "password"}],
-        [{"email_address": "test@test.com"}],
-        [{"email": "test@test.com", "pass": "password"}],
+        ({}, "Invalid payload"),
+        (
+            {"email": "test", "password": "password"},
+            "Please provide a valid email address",
+        ),
+        ({"email": "test@test.com"}, "Invalid payload"),
+        ({"password": "password"}, "Invalid payload"),
+        ({"email_address": "test@test.com"}, "Invalid payload"),
+        ({"email": "test@test.com", "pass": "password"}, "Invalid payload"),
     ],
 )
-def test_user_registration_invalid_payload(test_app, test_db, payload):
+def test_user_registration_invalid_payload(test_app, test_db, payload, message):
     recreate_db()
     client = test_app.test_client()
     py = payload
@@ -64,38 +67,38 @@ def test_user_registration_invalid_payload(test_app, test_db, payload):
     data = json.loads(res.data.decode())
     assert res.status_code == 400
     assert res.content_type == "application/json"
-    assert "Invalid payload" in data["message"]
+    assert message in data["message"]
     assert not data["status"]
 
 
-# def test_user_login(test_app, test_db, add_user):
-#     recreate_db()
-#     add_user("test@test.com", "password")
+def test_user_login(test_app, test_db, add_user):
+    recreate_db()
+    add_user("test@test.com", "password")
 
-#     client = test_app.test_client()
-#     res = client.post(
-#         f"{prefix}/auth/login",
-#         data=json.dumps({"email": "test@test.com", "password": "password"}),
-#         content_type="application/json",
-#     )
+    client = test_app.test_client()
+    res = client.post(
+        f"{prefix}/auth/login",
+        data=json.dumps({"email": "test@test.com", "password": "password"}),
+        content_type="application/json",
+    )
 
-#     data = json.loads(res.data.decode())
-#     assert res.status_code == 200
-#     assert res.content_type == "application/json"
-#     assert data["access_token"]
-#     assert data["refresh_token"]
+    data = json.loads(res.data.decode())
+    assert res.status_code == 200
+    assert res.content_type == "application/json"
+    assert data["access_token"]
+    assert data["refresh_token"]
 
 
-# def test_user_login_not_registered(test_app, test_db):
-#     client = test_app.test_client()
-#     res = client.post(
-#         f"{prefix}/auth/login",
-#         data=json.dumps({"email": "test@test.com", "password": "password"}),
-#         content_type="application/json",
-#     )
+def test_user_login_not_registered(test_app, test_db):
+    client = test_app.test_client()
+    res = client.post(
+        f"{prefix}/auth/login",
+        data=json.dumps({"email": "test@test.com", "password": "password"}),
+        content_type="application/json",
+    )
 
-#     data = json.loads(res.data.decode())
-#     assert res.status_code == 404
-#     assert res.content_type == "application/json"
-#     assert "User does not exist." in data["message"]
-#     assert not data["status"]
+    data = json.loads(res.data.decode())
+    assert res.status_code == 404
+    assert res.content_type == "application/json"
+    assert "User does not exist." in data["message"]
+    assert not data["status"]
