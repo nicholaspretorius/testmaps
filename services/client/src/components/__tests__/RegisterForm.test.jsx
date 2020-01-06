09bbcec252463f23b9d3a39673f9a3afe3ed88da
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-01-06 11:44:32 +0200

    tests: Update tests for Login, Register

--- services/client/src/components/__tests__/RegisterForm.test.jsx
+++ services/client/src/components/__tests__/RegisterForm.test.jsx
@@ -5,12 +5,33 @@ import RegisterForm from "./../RegisterForm";
 
 afterEach(cleanup);
 
+const props = {
+  handleRegisterFormSubmit: () => {
+    return true;
+  }
+};
+
 it("renders properly", () => {
-  const { getByText } = render(<RegisterForm />);
+  const { getByText } = render(<RegisterForm {...props} />);
   expect(getByText("Register")).toHaveClass("title");
 });
 
+it("renders with default props", () => {
+  const { getByLabelText, getByText } = render(<RegisterForm {...props} />);
+
+  const emailInput = getByLabelText("Email");
+  expect(emailInput).toHaveAttribute("type", "email");
+  expect(emailInput).not.toHaveValue();
+
+  const passwordInput = getByLabelText("Password");
+  expect(passwordInput).toHaveAttribute("type", "password");
+  expect(passwordInput).not.toHaveValue();
+
+  const buttonInput = getByText("Submit");
+  expect(buttonInput).toHaveValue("Submit");
+});
+
 it("renders", () => {
-  const { asFragment } = renderWithRouter(<RegisterForm />);
+  const { asFragment } = renderWithRouter(<RegisterForm {...props} />);
   expect(asFragment()).toMatchSnapshot();
 });
