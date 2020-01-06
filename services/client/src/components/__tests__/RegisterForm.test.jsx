bb84ddefa37ff8e679870a8866c4155376152a6b
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-01-06 13:39:21 +0200

    tests: Add validation tests

--- services/client/src/components/__tests__/RegisterForm.test.jsx
+++ services/client/src/components/__tests__/RegisterForm.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { cleanup, fireEvent } from "@testing-library/react";
+import { cleanup, fireEvent, wait } from "@testing-library/react";
 
 import RegisterForm from "./../RegisterForm";
 
@@ -47,8 +47,102 @@ describe("handles form validation correctly", () => {
     isAuthenticated: jest.fn()
   };
 
-  it("when fields are empty", async () => {});
-  it("when email field is not valid", async () => {});
-  it("when fields are not the correct length", async () => {});
-  it("when fields are valid", async () => {});
+  it("when fields are empty", async () => {
+    const { getByLabelText, container, findByTestId } = renderWithRouter(
+      <RegisterForm {...mockProps} />
+    );
+
+    const form = container.querySelector("form");
+    const emailInput = getByLabelText("Email");
+    const passwordInput = getByLabelText("Password");
+
+    expect(mockProps.onHandleRegisterFormSubmit).toHaveBeenCalledTimes(0);
+
+    fireEvent.blur(emailInput);
+    fireEvent.blur(passwordInput);
+
+    expect((await findByTestId("errors-email")).innerHTML).toBe("Email is required.");
+    expect((await findByTestId("errors-password")).innerHTML).toBe("Password is required.");
+
+    fireEvent.submit(form);
+
+    await wait(() => {
+      expect(mockProps.onHandleRegisterFormSubmit).toHaveBeenCalledTimes(0);
+    });
+  });
+
+  it("when email field is not valid", async () => {
+    const { getByLabelText, container, findByTestId } = renderWithRouter(
+      <RegisterForm {...mockProps} />
+    );
+
+    const form = container.querySelector("form");
+    const emailInput = getByLabelText("Email");
+
+    expect(mockProps.onHandleRegisterFormSubmit).toHaveBeenCalledTimes(0);
+
+    fireEvent.change(emailInput, { target: { value: "invalid" } });
+    fireEvent.blur(emailInput);
+
+    expect((await findByTestId("errors-email")).innerHTML).toBe("Please enter a valid email.");
+
+    fireEvent.submit(form);
+
+    await wait(() => {
+      expect(mockProps.onHandleRegisterFormSubmit).toHaveBeenCalledTimes(0);
+    });
+  });
+
+  it("when fields are not the correct length", async () => {
+    const { getByLabelText, container, findByTestId } = renderWithRouter(
+      <RegisterForm {...mockProps} />
+    );
+
+    const form = container.querySelector("form");
+    const emailInput = getByLabelText("Email");
+    const passwordInput = getByLabelText("Password");
+
+    expect(mockProps.onHandleRegisterFormSubmit).toHaveBeenCalledTimes(0);
+
+    fireEvent.change(emailInput, { target: { value: "a@b.c" } });
+    fireEvent.blur(emailInput);
+    fireEvent.change(passwordInput, { target: { value: "short" } });
+    fireEvent.blur(passwordInput);
+
+    expect((await findByTestId("errors-email")).innerHTML).toBe(
+      "Email must be 6 or more characters."
+    );
+    expect((await findByTestId("errors-password")).innerHTML).toBe(
+      "Password must be 8 or more characters."
+    );
+
+    fireEvent.submit(form);
+
+    await wait(() => {
+      expect(mockProps.onHandleRegisterFormSubmit).toHaveBeenCalledTimes(0);
+    });
+  });
+
+  it("when fields are valid", async () => {
+    const { getByLabelText, container, findByTestId } = renderWithRouter(
+      <RegisterForm {...mockProps} />
+    );
+
+    const form = container.querySelector("form");
+    const emailInput = getByLabelText("Email");
+    const passwordInput = getByLabelText("Password");
+
+    expect(mockProps.onHandleRegisterFormSubmit).toHaveBeenCalledTimes(0);
+
+    fireEvent.change(emailInput, { target: { value: "test@test.com" } });
+    fireEvent.blur(emailInput);
+    fireEvent.change(passwordInput, { target: { value: "password" } });
+    fireEvent.blur(passwordInput);
+
+    fireEvent.submit(form);
+
+    await wait(() => {
+      expect(mockProps.onHandleRegisterFormSubmit).toHaveBeenCalledTimes(1);
+    });
+  });
 });
