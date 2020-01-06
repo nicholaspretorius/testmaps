3abc803e1b3184c22928c0d275499944b7f756ae
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-01-06 09:51:12 +0200

    feat: Add NavBar and Register, Login forms

--- services/client/src/components/App.jsx
+++ services/client/src/components/App.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import axios from "axios";
 
+import NavBar from "./NavBar";
 import Users from "./Users";
 import AddUser from "./AddUser";
 import About from "./About";
+import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
 
 class App extends React.Component {
   state = {
     users: [],
-    email: ""
+    email: "",
+    title: "Testmaps"
   };
 
   getUsers() {
@@ -52,38 +56,45 @@ class App extends React.Component {
   }
 
   render() {
+    const { title } = this.state;
+
     return (
-      <section className="section">
-        <div className="container">
-          <div className="columns">
-            <div className="column is-one-third">
-              <br />
-              <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={() => (
-                    <div>
-                      <h1 className="title is-1 is-1">Users</h1>
-                      <hr />
-                      <br />
-                      <AddUser
-                        addUser={this.addUser}
-                        email={this.state.email}
-                        onHandleChange={this.handleChange}
-                      />
-                      <hr />
-                      <br />
-                      <Users users={this.state.users} />
-                    </div>
-                  )}
-                />
-                <Route exact path="/about" component={About} />
-              </Switch>
+      <div>
+        <NavBar title={title} />
+        <section className="section">
+          <div className="container">
+            <div className="columns">
+              <div className="column is-one-third">
+                <br />
+                <Switch>
+                  <Route component={LoginForm} />
+                  <Route component={RegisterForm} />
+                  <Route
+                    exact
+                    path="/"
+                    render={() => (
+                      <div>
+                        <h1 className="title is-1 is-1">Users</h1>
+                        <hr />
+                        <br />
+                        <AddUser
+                          addUser={this.addUser}
+                          email={this.state.email}
+                          onHandleChange={this.handleChange}
+                        />
+                        <hr />
+                        <br />
+                        <Users users={this.state.users} />
+                      </div>
+                    )}
+                  />
+                  <Route exact path="/about" component={About} />
+                </Switch>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     );
   }
 }
---
+++ services/client/src/components/LoginForm.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoginForm = () => {
+  return (
+    <div>
+      <h1 className="title is-1">Login</h1>
+      <hr />
+      <br />
+    </div>
+  );
+};
+
+export default LoginForm;
