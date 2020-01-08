475c32b6596f4b32100ddb4d89b48795309fccb9
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-01-08 13:44:47 +0200

    feat: Update Auth0 flow to use access_token

--- services/client/src/components/Auth0.jsx
+++ services/client/src/components/Auth0.jsx
@@ -7,13 +7,12 @@ class Auth {
       audience: process.env.REACT_APP_API_AUDIENCE,
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
       redirectUri: "http://localhost:3007/callback",
-      responseType: "id_token",
-      // scope: "openid profile"
-      scope: ""
+      responseType: "token",
+      scope: "openid profile email"
     });
 
-    this.getProfile = this.getProfile.bind(this);
     this.handleAuthentication = this.handleAuthentication.bind(this);
+    this.getProfile = this.getProfile.bind(this);
     this.isAuthenticated = this.isAuthenticated.bind(this);
     this.signIn = this.signIn.bind(this);
     this.signOut = this.signOut.bind(this);
@@ -27,6 +26,10 @@ class Auth {
     return this.idToken;
   }
 
+  getAccessToken() {
+    return this.accessToken;
+  }
+
   isAuthenticated() {
     return new Date().getTime() < this.expiresAt;
   }
@@ -36,18 +39,29 @@ class Auth {
   }
 
   handleAuthentication() {
-    console.log("Handle auth");
     return new Promise((resolve, reject) => {
       this.auth0.parseHash((err, authResult) => {
         if (err) return reject(err);
-        if (!authResult || !authResult.idToken) {
+        if (!authResult || !authResult.accessToken) {
           return reject(err);
         }
+        window.localStorage.setItem("access_token", authResult.accessToken);
+        resolve(authResult);
+      });
+    }).then(authResult => {
+      return new Promise((resolve, reject) => {
+        this.auth0.client.userInfo(authResult.accessToken, function(err, user) {
+          // Now you have the user's information
+          if (err) return reject(err);
+          resolve({ user, authResult });
+        });
+      }).then(data => {
+        console.log("Data: ", data);
+        const { user, authResult } = data;
+        this.profile = user;
+        this.accessToken = authResult.accessToken;
         this.idToken = authResult.idToken;
-        this.profile = authResult.idTokenPayload;
-        // set the time that the id token will expire at
-        this.expiresAt = authResult.idTokenPayload.exp * 1000;
-        resolve();
+        this.expiresAt = new Date().getTime() + authResult.expiresIn;
       });
     });
   }
@@ -57,6 +71,7 @@ class Auth {
     this.idToken = null;
     this.profile = null;
     this.expiresAt = null;
+    window.localStorage.removeItem("access_token");
   }
 }
 
