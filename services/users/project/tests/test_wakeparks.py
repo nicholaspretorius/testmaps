3f994ffca02c9e0c2e0da940145b029ca276eacb
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-01-13 14:39:27 +0200

    tests: Add test for GET /wakeparks/:id

--- services/users/project/tests/test_wakeparks.py
+++ services/users/project/tests/test_wakeparks.py
@@ -24,4 +24,42 @@ def test_get_all_wakeparks(test_app, test_db, add_wakepark):
     res = client.get(f"/wakeparks/")
     data = json.loads(res.data.decode())
     assert res.status_code == 200
+    assert res.content_type == "application/json"
     assert len(data) == 2
+
+
+def test_get_single_wakepark(test_app, test_db, add_wakepark):
+    recreate_db()
+    client = test_app.test_client()
+    wakepark = add_wakepark(
+        "Stoke City Wakepark",
+        "The only cablepark in Gauteng!",
+        -25.952558,
+        28.185543,
+        "stokecitywake",
+    )
+    client = test_app.test_client()
+    res = client.get(f"/wakeparks/{wakepark.id}")
+    data = json.loads(res.data.decode())
+    assert res.status_code == 200
+    assert res.content_type == "application/json"
+    assert "Stoke City Wakepark" in data["name"]
+    assert 1 == int(data["id"])
+
+
+def test_get_single_wakepark_not_found(test_app, test_db):
+    client = test_app.test_client()
+    res = client.get(f"/wakeparks/999")
+    data = json.loads(res.data.decode())
+    assert res.status_code == 404
+    assert not data["status"]
+    assert "Resource not found" in data["message"]
+
+
+def test_single_wakepark_no_id(test_app, test_db):
+    client = test_app.test_client()
+    res = client.get(f"/wakeparks/test")
+    data = json.loads(res.data.decode())
+    assert res.status_code == 404
+    assert not data["status"]
+    assert "Resource not found" in data["message"]
