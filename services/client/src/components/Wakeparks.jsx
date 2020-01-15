fac529d1ec0bec5c2dce6a42babe26f47f68040a
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-01-15 13:25:20 +0200

    feat: Add GET /wakeparks

--- services/client/src/components/Wakeparks.jsx
+++ services/client/src/components/Wakeparks.jsx
@@ -1,11 +1,51 @@
 import React from "react";
+import axios from "axios";
 
-const Wakeparks = () => {
-  return (
-    <div>
-      <h3 className="title">Wakeparks</h3>
-    </div>
-  );
-};
+class Wakeparks extends React.Component {
+  state = {
+    wakeparks: null
+  };
+
+  componentDidMount() {
+    this.getWakeparks();
+  }
+
+  async getWakeparks() {
+    const options = {
+      url: `${process.env.REACT_APP_USERS_SERVICE_URL}/wakeparks`,
+      methods: "get",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    return axios(options)
+      .then(res => {
+        this.setState({ wakeparks: res.data });
+      })
+      .catch(err => {
+        // console.log(err);
+      });
+  }
+
+  render() {
+    const { wakeparks } = this.state;
+
+    return (
+      <div>
+        <h3 className="title">Wakeparks</h3>
+
+        <ul>
+          {wakeparks &&
+            wakeparks.map(wakepark => (
+              <li key={wakepark.id} data-testid="wakepark-name">
+                {wakepark.name}
+              </li>
+            ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Wakeparks;
--- services/client/src/components/__tests__/Wakeparks.test.jsx
+++ services/client/src/components/__tests__/Wakeparks.test.jsx
@@ -1,11 +1,43 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { cleanup, wait } from "@testing-library/react";
+import axios from "axios";
 
 import Wakeparks from "./../Wakeparks";
 
 afterEach(cleanup);
 
-it("renders a title", () => {
-  const { getByText } = render(<Wakeparks />);
+jest.mock("axios");
+
+axios.mockImplementationOnce(() =>
+  Promise.resolve({
+    data: [
+      {
+        id: "1",
+        name: "Stoke City Wakepark",
+        description: "The only cable wakepark in Gauteng!",
+        location: {
+          lat: -25.952558,
+          lng: 28.185543
+        },
+        social: {
+          instagram: "https://www.instagram.com/stokecitywake"
+        }
+      }
+    ]
+  })
+);
+
+it("renders a title", async () => {
+  const { getByText } = renderWithRouter(<Wakeparks />);
   expect(getByText("Wakeparks")).toHaveClass("title");
+});
+
+xit("renders a list of wakeparks", async () => {
+  const { findByTestId } = renderWithRouter(<Wakeparks />);
+
+  await wait(() => {
+    expect(axios).toHaveBeenCalledTimes(1);
+  });
+
+  expect((await findByTestId("wakepark-name")).innerHTML).toBe("Stoke City Wakepark");
 });
