724bcfeaf8347c79bcb8b4d98126f3b9866228b6
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-01-21 09:26:02 +0200

    test: Fix Callback test for onHandleAuth call

--- services/client/src/components/__tests__/Callback.test.jsx
+++ services/client/src/components/__tests__/Callback.test.jsx
@@ -1,26 +1,20 @@
 import React from "react";
-import { cleanup, wait } from "@testing-library/react";
+import { cleanup } from "@testing-library/react";
 
 import Callback from "./../Callback";
-import localStorage from "../../services/localStorage";
 
 afterEach(cleanup);
 
-localStorage.setItem = jest.fn();
+jest.mock("../../services/localStorage", () => ({
+  setItem: jest.fn()
+}));
 
 const props = {
-  onHandleAuth: () => {
-    return true;
-  }
+  onHandleAuth: jest.fn()
 };
 
 it("should render", () => {
   const { getByText } = renderWithRouter(<Callback {...props} />);
   expect(getByText("Loading profile...")).toBeVisible();
-});
-
-xit("should set accessToken in localStorage", async () => {
-  await wait(() => {
-    expect(localStorage.setItem).toBeCalledWith("accessToken");
-  });
+  expect(props.onHandleAuth).toHaveBeenCalledTimes(1);
 });
