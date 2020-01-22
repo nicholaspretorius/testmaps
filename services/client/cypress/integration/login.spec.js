a25657893a52cb579e435c367d59ea8b67562b90
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-01-22 13:38:59 +0200

    tests: Cypress login

---
+++ services/client/cypress/integration/login.spec.js
@@ -0,0 +1,25 @@
+describe("login", () => {
+  it("should login successfully", () => {
+    cy.login()
+      .then(res => {
+        console.log("Cy login res: ", res);
+        return res.body;
+      })
+      .then(data => {
+        const { access_token, expires_in, id_token } = data;
+
+        const auth0State = {
+          nonce: "",
+          state: "test-state"
+        };
+
+        const callbackUrl = `/callback#access_token=${access_token}&scope=openid&id_token=${id_token}&expires_in=${expires_in}&token_type=Bearer&state=${auth0State.state}`;
+        // const callbackUrl = `/callback#access_token=${access_token}&scope=openid&id_token=${id_token}&expires_in=${expires_in}&token_type=Bearer&state=${auth0state}`;
+        cy.visit(callbackUrl, {
+          onBeforeLoad(win) {
+            win.document.cookie = "com.auth0.auth.test-state=" + JSON.stringify(auth0State);
+          }
+        });
+      });
+  });
+});
