c5b4d62f2ae31a4d7fe04f3a76c3a677582f7381
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-01-22 13:38:45 +0200

    tests: Home page first pass

--- services/client/cypress/integration/app.spec.js
+++ services/client/cypress/integration/app.spec.js
@@ -1,6 +1,27 @@
-describe("App", () => {
-  it("loads", () => {
-    cy.visit("/");
-    cy.get("h3").contains("Wakeparks");
+describe("App Home Page", () => {
+  context("MacBoook Pro 15", () => {
+    beforeEach(() => {
+      cy.viewport(1200, 900);
+    });
+
+    it("visits the page", () => {
+      cy.visit("/");
+    });
+
+    it("displays the page title", () => {
+      cy.visit("/")
+        .get("h3")
+        .contains("Wakeparks");
+    });
+
+    it("displays the logged out navbar view", () => {
+      cy.visit("/")
+        .get("a.nav-title")
+        .contains("Wakemaps")
+        .get("a.navbar-item")
+        .contains("About")
+        .get("span.navbar-item")
+        .contains("Login");
+    });
   });
 });
