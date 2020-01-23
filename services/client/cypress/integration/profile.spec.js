5e46784599fcc07d52e8b2c13e238638081c56b6
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-01-23 14:13:03 +0200

    tests: Fix e2e for /profile

--- services/client/cypress/integration/profile.spec.js
+++ services/client/cypress/integration/profile.spec.js
@@ -3,64 +3,85 @@ describe("profile page", () => {
     cy.viewport(1200, 900);
   });
 
-  it("should login successfully", () => {
-    cy.login()
-      .then(res => {
-        return res.body;
-      })
-      .then(data => {
-        const { access_token, expires_in, id_token } = data;
+  const baseUrl = Cypress.config().baseUrl;
 
-        const auth0State = {
-          nonce: "",
-          state: "test-state"
-        };
+  // logged out
+  describe("when logged out", () => {
+    it("visits the page and displays 404 page", () => {
+      cy.visit("/profile")
+        .url()
+        .should("eq", baseUrl + "/profile");
 
-        const callbackUrl = `/callback#access_token=${access_token}&scope=openid&id_token=${id_token}&expires_in=${expires_in}&token_type=Bearer&state=${auth0State.state}`;
+      cy.get("h3.title").contains("404 Not Found.");
 
-        cy.visit(callbackUrl, {
-          onBeforeLoad(win) {
-            win.document.cookie = "com.auth0.auth.test-state=" + JSON.stringify(auth0State);
-          }
+      cy.get("[data-testid=not-found]").contains("Unfortunately there is no page with that url.");
+    });
+  });
+
+  // regular user
+  describe("when logged in as regular user", () => {
+    const user = Cypress.env("auth_user_username");
+    const pass = Cypress.env("auth_user_password");
+
+    it("should login successfully", () => {
+      cy.login(user, pass)
+        .then(res => {
+          return res.body;
+        })
+        .then(data => {
+          const { access_token, expires_in, id_token } = data;
+
+          const auth0State = {
+            nonce: "",
+            state: "test-state"
+          };
+
+          const callbackUrl = `/callback#access_token=${access_token}&scope=openid&id_token=${id_token}&expires_in=${expires_in}&token_type=Bearer&state=${auth0State.state}`;
+
+          cy.visit(callbackUrl, {
+            onBeforeLoad(win) {
+              win.document.cookie = "com.auth0.auth.test-state=" + JSON.stringify(auth0State);
+            }
+          });
         });
-      });
-  });
+    });
 
-  it("displays the profile page", () => {
-    cy.get("[data-testid=nav-profile]")
-      .click()
-      .url()
-      .should("include", "/profile");
-  });
+    it("displays the profile page", () => {
+      cy.get("[data-testid=nav-profile]")
+        .click()
+        .url()
+        .should("include", "/profile");
+    });
 
-  it("displays the title", () => {
-    cy.get("h3.title").contains("Profile");
-  });
+    it("displays the title", () => {
+      cy.get("h3.title").contains("Profile");
+    });
 
-  it("displays the name, email and sub", () => {
-    cy.get("div.user-profile>ul>li")
-      .eq(0)
-      .contains("Name: ");
-    cy.get("div.user-profile>ul>li")
-      .eq(1)
-      .contains("Email: ");
-    cy.get("div.user-profile>ul>li")
-      .eq(2)
-      .contains("Sub: ");
-  });
+    it("displays the name, email and sub", () => {
+      cy.get("div.user-profile>ul>li")
+        .eq(0)
+        .contains("Name: ");
+      cy.get("div.user-profile>ul>li")
+        .eq(1)
+        .contains("Email: ");
+      cy.get("div.user-profile>ul>li")
+        .eq(2)
+        .contains("Sub: ");
+    });
 
-  it("displays the accessToken", () => {
-    cy.get("[data-testid=access-token]");
-  });
+    it("displays the accessToken", () => {
+      cy.get("[data-testid=access-token]");
+    });
 
-  it("displays the permissions", () => {
-    cy.get("[data-testid]=permissions");
-  });
+    it("displays the permissions", () => {
+      cy.get("[data-testid=permissions]");
+    });
 
-  it("logs the user out", () => {
-    cy.get("[data-testid=nav-signout]")
-      .click()
-      .url()
-      .should("eq", Cypress.config().baseUrl + "/");
+    it("logs the user out", () => {
+      cy.get("[data-testid=nav-signout]")
+        .click()
+        .url()
+        .should("eq", Cypress.config().baseUrl + "/");
+    });
   });
 });
--- services/client/src/components/NotFound.jsx
+++ services/client/src/components/NotFound.jsx
@@ -4,11 +4,9 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div>
-      <h3>404 Not Found.</h3>
+      <h3 className="title">404 Not Found.</h3>
       <div>
-        <p data-testid="not-found">
-          Unfortunately there is no page with that url.
-        </p>
+        <p data-testid="not-found">Unfortunately there is no page with that url.</p>
         <Link to="/">Home</Link>
       </div>
     </div>
