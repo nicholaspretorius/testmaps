5ff77deaddfa42c754918983f196b9b9ad156735
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-01-23 14:20:53 +0200

    tests: Add parkadmin permissions e2e test

--- services/client/cypress/integration/profile.spec.js
+++ services/client/cypress/integration/profile.spec.js
@@ -19,7 +19,7 @@ describe("profile page", () => {
   });
 
   // regular user
-  describe("when logged in as regular user", () => {
+  describe("when logged in as a regular user", () => {
     const user = Cypress.env("auth_user_username");
     const pass = Cypress.env("auth_user_password");
 
@@ -78,6 +78,83 @@ describe("profile page", () => {
     });
 
     it("logs the user out", () => {
+      cy.get("[data-testid=nav-signout]")
+        .click()
+        .url()
+        .should("eq", Cypress.config().baseUrl + "/");
+    });
+  });
+
+  // parkadmin user
+  describe("when logged in as a parkadmin user", () => {
+    const user = Cypress.env("auth_parkadmin_username");
+    const pass = Cypress.env("auth_parkadmin_password");
+
+    it("should login successfully", () => {
+      cy.login(user, pass)
+        .then(res => {
+          return res.body;
+        })
+        .then(data => {
+          const { access_token, expires_in, id_token } = data;
+
+          const auth0State = {
+            nonce: "",
+            state: "test-state"
+          };
+
+          const callbackUrl = `/callback#access_token=${access_token}&scope=openid&id_token=${id_token}&expires_in=${expires_in}&token_type=Bearer&state=${auth0State.state}`;
+
+          cy.visit(callbackUrl, {
+            onBeforeLoad(win) {
+              win.document.cookie = "com.auth0.auth.test-state=" + JSON.stringify(auth0State);
+            }
+          });
+        });
+    });
+
+    it("displays the profile page", () => {
+      cy.get("[data-testid=nav-profile]")
+        .click()
+        .url()
+        .should("include", "/profile");
+    });
+
+    it("displays the title", () => {
+      cy.get("h3.title").contains("Profile");
+    });
+
+    it("displays the name, email and sub", () => {
+      cy.get("div.user-profile>ul>li")
+        .eq(0)
+        .contains("Name: ");
+      cy.get("div.user-profile>ul>li")
+        .eq(1)
+        .contains("Email: ");
+      cy.get("div.user-profile>ul>li")
+        .eq(2)
+        .contains("Sub: ");
+    });
+
+    it("displays the accessToken", () => {
+      cy.get("[data-testid=access-token]");
+    });
+
+    it("displays the permissions", () => {
+      cy.get("[data-testid=permissions]>li")
+        .eq(0)
+        .contains("get:cablepark");
+
+      cy.get("[data-testid=permissions]>li")
+        .eq(1)
+        .contains("post:cablepark");
+
+      cy.get("[data-testid=permissions]>li")
+        .eq(2)
+        .contains("put:cablepark");
+    });
+
+    xit("logs the user out", () => {
       cy.get("[data-testid=nav-signout]")
         .click()
         .url()
